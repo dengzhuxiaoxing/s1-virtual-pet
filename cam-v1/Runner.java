78e8ae9eb688dca4b8b3c4a6e141797bb6ae5619
dengzhuxiaoxing <142451986+dengzhuxiaoxing@users.noreply.github.com>
2023-10-12 21:31:45 -0700

    Update Runner.java

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,7 +1,56 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
+
+
+
 public class Runner {
     public static void main(String[] args) {
         VirtualPet v = new VirtualPet();
-        //v.sleep();
-        v.exercise();
+        new Runner();
+  }
+
+
+  enum activities {
+    exercise,
+    feed
+  }
+
+
+  public Runner() {
+    // Main
+    VirtualPet p = new VirtualPet();
+
+
+    // Tasks
+    p.exercise();
+    wait(1000);
+    p.feed();
+    wait(1000);
+    p.exercise();
+    String ans = getResponse("How was your day?");
+    System.out.println(ans);
+  }
+
+
+  public String getResponse(String prompt) {
+    String res = (String)JOptionPane.showInputDialog(
+      new JFrame(),
+      prompt,
+      "Response",
+      JOptionPane.PLAIN_MESSAGE
+    );
+    return res;
+  }
+
+
+  public void wait(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (Exception e) {
+      // TODO: handle exception
     }
+  }
+
+
 }
