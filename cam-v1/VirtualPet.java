5733f56c5c056df78f1abc9bdc717c21a8390a42
dengzhuxiaoxing <142451986+dengzhuxiaoxing@users.noreply.github.com>
2023-10-12 21:36:07 -0700

    Update VirtualPet.java

--- cam-v1/VirtualPet.java
+++ cam-v1/VirtualPet.java
@@ -4,17 +4,18 @@
  * @author ?
  */
 public class VirtualPet {
-    
+   
     VirtualPetFace face;
-    int hunger = 0;   // how hungry the pet is.
-    
+    private int hunger = 0;   // how hungry the pet is.
+    private int tiredness = 0;
+   
     // constructor
     public VirtualPet() {
         face = new VirtualPetFace();
         face.setImage("normal");
         face.setMessage("Hello.");
     }
-    
+   
     public void feed() {
         if (hunger > 10) {
             hunger = hunger - 10;
@@ -24,16 +25,23 @@ public class VirtualPet {
         face.setMessage("Yum, thanks");
         face.setImage("normal");
     }
-    
+   
     public void exercise() {
         hunger = hunger + 3;
+        tiredness += 2;
         face.setMessage("1, 2, 3, jump.  Whew.");
         face.setImage("tired");
+        if (tiredness > 4) {
+            face.setMessage("That was great.");
+            face.setImage("ecstatic");
+           
+        }
     }
-    
+   
     public void sleep() {
         hunger = hunger + 1;
         face.setImage("asleep");
     }
+
 
 } // end Virtual Pet
